4528f5faa36173488763e20c8450475f454eea81
unknown <IOT-STU@1.1>
2018-05-03 12:40:39 +0800

    第八周指纹识别作业

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Collecting = new System.Windows.Forms.Button();
+            this.Verify = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pictureBox0 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.axZKFPEngX1 = new AxZKFPEngXControl.AxZKFPEngX();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axZKFPEngX1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(349, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(297, 272);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Collecting
+            // 
+            this.Collecting.Location = new System.Drawing.Point(346, 307);
+            this.Collecting.Name = "Collecting";
+            this.Collecting.Size = new System.Drawing.Size(75, 46);
+            this.Collecting.TabIndex = 1;
+            this.Collecting.Text = "指纹登记";
+            this.Collecting.UseVisualStyleBackColor = true;
+            this.Collecting.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Verify
+            // 
+            this.Verify.Location = new System.Drawing.Point(436, 307);
+            this.Verify.Name = "Verify";
+            this.Verify.Size = new System.Drawing.Size(75, 46);
+            this.Verify.TabIndex = 2;
+            this.Verify.Text = "比对指纹";
+            this.Verify.UseVisualStyleBackColor = true;
+            this.Verify.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // pictureBox0
+            // 
+            this.pictureBox0.Image = global::WindowsFormsApplication1.Properties.Resources.关;
+            this.pictureBox0.Location = new System.Drawing.Point(528, 307);
+            this.pictureBox0.Name = "pictureBox0";
+            this.pictureBox0.Size = new System.Drawing.Size(118, 46);
+            this.pictureBox0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox0.TabIndex = 4;
+            this.pictureBox0.TabStop = false;
+            this.pictureBox0.Click += new System.EventHandler(this.pictureBox0_Click_1);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(347, 371);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 16);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "NO.1道闸";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(528, 371);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 16);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "NO.2道闸";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(320, 336);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // axZKFPEngX1
+            // 
+            this.axZKFPEngX1.Enabled = true;
+            this.axZKFPEngX1.Location = new System.Drawing.Point(254, 364);
+            this.axZKFPEngX1.Name = "axZKFPEngX1";
+            this.axZKFPEngX1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axZKFPEngX1.OcxState")));
+            this.axZKFPEngX1.Size = new System.Drawing.Size(75, 23);
+            this.axZKFPEngX1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 412);
+            this.Controls.Add(this.axZKFPEngX1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox0);
+            this.Controls.Add(this.Verify);
+            this.Controls.Add(this.Collecting);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axZKFPEngX1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Collecting;
+        private System.Windows.Forms.Button Verify;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox0;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private AxZKFPEngXControl.AxZKFPEngX axZKFPEngX1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,213 @@
+﻿//单元3：指纹门禁
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //声明数据库操作相关变量
+        OleDbConnection conn = new OleDbConnection();
+        OleDbCommand comm = new OleDbCommand();
+        OleDbDataAdapter adapter = new OleDbDataAdapter();
+        DataSet ds = new DataSet();
+
+        int DC;//设备状态
+        bool DC_switch = false;//指纹是否开启
+        int EnrollNum = 0;
+
+        //表单装入事件，连接数据库
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //创建并打开连接
+            conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Database1.accdb;Persist Security Info=False;";
+            conn.Open();
+            comm.Connection = conn;
+            //执行查询命令
+            comm.CommandText = "select * from stuffTab";
+            adapter.SelectCommand = comm;
+            OleDbCommandBuilder builder = new OleDbCommandBuilder(adapter);
+            //为表格控件建立数据源
+            ds.Clear();
+            adapter.Fill(ds, "stuffTab");
+            dataGridView1.DataSource = ds.Tables["stuffTab"];
+            //OpenDoor();
+        }
+
+
+        //比对指纹
+        private void Verify_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开始采集指纹...请将指纹按压指纹指纹仪1次...");
+            axZKFPEngX1.BeginCapture();
+        }
+
+        //指纹登记OnEnroll事件
+        private void axZKFPEngX1_OnEnroll(object sender, AxZKFPEngXControl.IZKFPEngXEvents_OnEnrollEvent e)
+        {
+            string fileName = "";
+            axZKFPEngX1.CancelEnroll();//关闭登记状态
+            if (!e.actionResult)
+            {
+                MessageBox.Show("登记指纹失败！");
+                return;
+            }
+
+            EnrollNum++;
+            fileName = "FingerPic" + EnrollNum + ".JPG";
+            axZKFPEngX1.SaveJPG(fileName);//保存指纹图像
+            pictureBox1.Image = Image.FromFile(fileName);
+            string mb = axZKFPEngX1.GetTemplateAsString();//得到字符串格式的特征模板
+
+            dataGridView1.CurrentRow.Cells["指纹"].Value = mb;
+            adapter.Update(ds, "stuffTab");
+
+            MessageBox.Show("登记指纹成功！");
+        }
+
+        //指纹比对OnCapture事件
+        private void axZKFPEngX1_OnCapture(object sender, AxZKFPEngXControl.IZKFPEngXEvents_OnCaptureEvent e)
+        {
+            string fileName = "";
+            axZKFPEngX1.CancelCapture();//关闭取验证指纹状态
+            if (!e.actionResult)
+            {
+                MessageBox.Show("取验证模板失败！");
+                return;
+            }
+
+            EnrollNum++;
+            fileName = "FingerPic" + EnrollNum + ".JPG";
+            axZKFPEngX1.SaveJPG(fileName);//保存指纹图像
+            pictureBox1.Image = Image.FromFile(fileName);//显示指纹
+            string mb = axZKFPEngX1.GetTemplateAsString();//得到字符串格式的特征模板
+
+            bool flag = false;
+            bool chang = false;
+            for (int k = 0; k < dataGridView1.RowCount-1 ; k++)
+            {
+                string fingerFile = dataGridView1.Rows[k].Cells["指纹"].Value.ToString();
+                if (axZKFPEngX1.VerFingerFromStr(ref fingerFile, mb, false, ref chang))
+                {
+                    MessageBox.Show("比对成功!");
+                    //插入开启门禁
+                    OpenDoor();
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                MessageBox.Show("比对失败！");
+            }
+        }
+
+        //打开道闸
+        private void OpenDoor()
+        {
+            TcpClient client = new TcpClient();//定义一个客户端
+
+            client.Connect("127.0.0.1", 60006);//客户端连接地址
+
+            NetworkStream ns = client.GetStream();//定义一个客户端流
+            //打开道闸信号
+            string dz;
+            if(radioButton1.Checked)
+            {
+                dz = "N3000 -USER \"abc\" -PASSWORD \"123\" -OPEN \"m001-1号\"";
+            }
+            else
+            {
+                dz = "N3000 -USER \"abc\" -PASSWORD \"123\" -OPEN \"m002-1号\"";
+            }
+            Byte[] dat = Encoding.Default.GetBytes(dz);  //m001  m002
+            //发送信号到服务器，由服务器远程打开道闸
+            ns.Write(dat, 0, dat.Length);
+            client.Close();//关闭客户端
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox0_Click_1(object sender, EventArgs e)
+        {
+            if (!DC_switch)
+            {
+                DC = axZKFPEngX1.InitEngine();
+                if (DC == 0)
+                {
+                    pictureBox0.Image = Image.FromFile("开.jpg");
+                    DC_switch = true;
+                    MessageBox.Show("指纹识别仪初始化成功！");
+                    Collecting.Enabled = true;
+                    Verify.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("指纹识别仪初始化失败！");
+                    return;
+                }
+            }
+            else
+            {
+                axZKFPEngX1.EndEngine();
+                pictureBox0.Image = Image.FromFile("关.jpg");
+                DC_switch = false;
+                MessageBox.Show("指纹仪关闭成功！");
+                Collecting.Enabled = false;
+                Verify.Enabled = false;
+            }
+        }
+        //比对指纹
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开始采集指纹...请将指纹按压指纹指纹仪1次...");
+            axZKFPEngX1.BeginCapture();
+        }
+       
+        //指纹登记
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string NAME = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            DialogResult dr = MessageBox.Show("确认登记人是" + NAME + "吗？", "提示", MessageBoxButtons.OKCancel);
+            if (dr == DialogResult.Cancel) return;
+
+            MessageBox.Show("开始采集指纹...请将指纹按压指纹指纹仪3次...");
+            axZKFPEngX1.BeginEnroll();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
+
